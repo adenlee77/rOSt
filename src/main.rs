c491ed43cf6b47a10114bff49fa3be512bcd1150
adenlee77 <adenleeuw@gmail.com>
2025-11-22 15:45:22 -0500

    qemu closes after cargo test runs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
 #![no_std] // don't link the rust standard library
 #![no_main] // disable all rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"] // change name to something different than main
 
 mod vga_buffer;
 
 use core::panic::PanicInfo;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
 // this function is called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop{}
 }
 
@@ -16,5 +36,25 @@ pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function named '_start' by default
     println!("Hello World{}", "!");
 
+    #[cfg(test)] // allows cargo test to call renamed function through _start
+    test_main();
+
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
